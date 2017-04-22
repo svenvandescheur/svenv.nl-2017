54e900f447777fcefe19a2745b1e9762334c5242
Sven van de Scheur <svenvandescheur@gmail.com>
2017-04-23 00:16:00 +0200

    Remove search app for now.

--- svenv/svenv/urls.py
+++ svenv/svenv/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework import routers
-from search import views as search_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
@@ -23,8 +22,6 @@ urlpatterns = [
 
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
-    url(r'^search/$', search_views.search, name='search'),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
